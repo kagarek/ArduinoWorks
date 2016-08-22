ecda35c30cf49eb3ec90d9d0958872ca726404e4
KagareK <KagareK@yandex.ru>
2016-08-22 16:08:47 +0300

    Optimized button states

--- projects/_12.railway/_12.railway.ino
+++ projects/_12.railway/_12.railway.ino
@@ -1,10 +1,10 @@
 //#include <Servo.h>
+//Servo servo;
 int led1 = 8;
 int led2 = 7;
 int buttonON = 12;
-//Servo servo;
 boolean switcher = false;
-
+//int flag = 0;
 
 void setup() {
   //servo.attach(10);
@@ -14,14 +14,25 @@ void setup() {
 }
 
 void loop() {
-//button
-  checkSwitch();
 
-  if (switcher){
-    enableSemafor1();
-    enableSemafor2();
+checkSwitch();
+enableSemafors();
+
+}
+
+/*
+  if(digitalRead(buttonON)==HIGH && flag==0) //если кнопка нажата и перемення flag равна 0 , то ...
+  {
+    flag=1;
+    //это нужно для того что бы с каждым нажатием кнопки происходило только одно действие
   }
-  
+
+  if(digitalRead(buttonON)==LOW && flag==1) //если кнопка НЕ нажата и переменная flag равна - 1 ,то ...
+  {
+    enableSemafors();
+    flag=0;//обнуляем переменную flag
+  }
+*/
 //servo
 //servo.write(45);
 //delay(3000);
@@ -29,25 +40,22 @@ void loop() {
 //delay(3000);
 }
 
-void enableSemafor1(){
+void enableSemafors(){
+  while (switcher) {
     digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    checkSwitch();
     delay(250);
     digitalWrite(led1, LOW);
-    delay(250);
-    checkSwitch();
-}
-
-void enableSemafor2(){
     digitalWrite(led2, HIGH);
-    delay(250);
-    digitalWrite(led2, LOW);
-    delay(250);
     checkSwitch();
+    delay(250);
+  }
 }
 
 // need to keep button around 1 sec to disable everything
 void checkSwitch(){
-  if (digitalRead(buttonON)== HIGH){
+  if (digitalRead(buttonON) == HIGH){
     switcher = !switcher;
   }
 }
